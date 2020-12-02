a41f41a7b1fff2d293a76a49286e4752faebe7a2
mogo <2083491778@qq.com>
2020-12-02 14:28:45 +0800

    加入Hystrix服务熔断和HystrixDashboard监视面板

---
+++ hello-dubbo-service-user-consumer/src/main/java/com/byx/hello/dubbo/service/user/consumer/config/HystrixDashboardConfig.java
@@ -0,0 +1,23 @@
+package com.byx.hello.dubbo.service.user.consumer.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author wang zhen xing
+ * @date 2020/12/2 13:47
+ */
+@Configuration
+public class HystrixDashboardConfig {
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
